6d372fa9ff6d0de15cd57ecde33fb1fd1917f051
Mário Santos <mariodev7@gmail.com>
2024-04-25 20:39:50 -0300

    feat(types): add more events to api key log

--- src/types/ApiKeyLogTypes.ts
+++ src/types/ApiKeyLogTypes.ts
@@ -13,4 +13,6 @@ export const ApiKeyLogEvents = {
     API_KEY_ACTIVATED: { code: 'API_KEY_ACTIVATED', message: 'An api key was activated.' },
     API_KEY_DEACTIVATED: { code: 'API_KEY_DEACTIVATED', message: 'An api key was deactivated.' },
     API_KEY_REVOKED: { code: 'API_KEY_REVOKED', message: 'An api key was revoked.' },
+    API_KEY_VALIDATED: { code: 'API_KEY_VALIDATED', message: 'An api key was validated.' },
+    API_KEY_USED: { code: 'API_KEY_USED', message: 'An api key was used.' },
 }
