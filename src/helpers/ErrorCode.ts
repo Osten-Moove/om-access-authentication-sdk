ce54b651e35d8638081bc6f822a40adee695096e
Mário Santos <mariodev7@gmail.com>
2024-04-25 20:39:06 -0300

    feat(helpers): add more error code and error message handler

--- src/helpers/ErrorCode.ts
+++ src/helpers/ErrorCode.ts
@@ -6,4 +6,18 @@ export enum ErrorCode {
   PASSWORD_INVALID = 'PASSWORD_INVALID',
 
   API_KEY_NOT_FOUND = 'API_KEY_NOT_FOUND',
+  API_KEY_DEACTIVATED = 'API_KEY_DEACTIVATED',
+  PUBLIC_KEY_AND_SECRET_KEY_NOT_GIVEN = 'PUBLIC_KEY_AND_SECRET_KEY_NOT_GIVEN'
+}
+
+export const ErrorMessage: Record<ErrorCode, string> = {
+    LOGIN_NOT_FOUND: 'Login not found.',
+    LOGIN_NOT_VALID: 'Login not valid.',
+    LOGIN_ROLE_INVALID: 'Login role invalid.',
+    LOGIN_ALREADY_USED: 'Login already used.',
+    PASSWORD_INVALID: 'Password invalid.',
+
+    API_KEY_NOT_FOUND: 'Api key not found.',
+    API_KEY_DEACTIVATED: 'Api key deactivated.',
+    PUBLIC_KEY_AND_SECRET_KEY_NOT_GIVEN: 'You must give a public key or a secret key to validate.'
 }
