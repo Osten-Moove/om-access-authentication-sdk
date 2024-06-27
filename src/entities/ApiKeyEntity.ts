fab7b89f11f52b69bf1e436ad7aa3c1d40568b68
vanei2002 <vanei.jesus016@gmail.com>
2024-06-27 15:57:12 -0300

    feat(api-key): add ApiKeyEntity and ApiKeyService

---
+++ src/entities/ApiKeyEntity.ts
@@ -0,0 +1,44 @@
+import { randomUUID } from 'node:crypto'
+import * as bcrypt from 'bcrypt'
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity({ name: 'api_keys' })
+export class ApiKeyEntity<T extends string = string> {
+  @PrimaryColumn('uuid')
+  id: string
+
+  @Column({ type: 'uuid', name: 'secret_key', unique: true })
+  secretKey: string
+  
+
+  @Column({ type: 'boolean', name: 'is_active', default: true })
+  isActive: boolean
+
+  @Column({ type: 'character varying', name: 'alias', nullable: true })
+  alias: string
+
+  @Column({ type: 'character varying', array: true, name: 'roles', nullable: true })
+  roles: Array<T>
+
+  @CreateDateColumn({ type: 'timestamp without time zone', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
+  createdAt: string
+
+  @UpdateDateColumn({ type: 'timestamp without time zone', default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
+  updatedAt: string
+
+  constructor(entity?: {
+    isActive?: boolean
+    alias?: string
+    roles?: Array<T>
+    apiKey?: string;
+
+  }) {
+    if (!entity) return
+    if (entity.alias) this.alias = entity.alias
+    if (entity.isActive) this.isActive = entity.isActive
+    if (entity.roles) this.roles = entity.roles
+    if (entity.apiKey) this.secretKey = bcrypt.hashSync(entity.apiKey, 10)
+      
+    this.id = randomUUID()
+  }
+}
---
+++ src/services/ApiKeyService.ts
@@ -0,0 +1,140 @@
+import { Inject, Injectable } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+import { randomUUID } from 'node:crypto'
+import { ErrorCode } from '../helpers/ErrorCode'
+import { AuthenticationModule } from '../module/AuthenticationModule'
+import { ApiKeyLogService } from './ApiKeyLogService'
+import type { Repository } from 'typeorm'
+import { ApiKeyEntity } from '../entities/ApiKeyEntity'
+import { ApiKeyOptionsRequest, GenerateApiKeyRequest } from '../types/ApiKeyTypes'
+import { ApiKeyLogEvents } from '../types/ApiKeyLogTypes'
+
+
+
+
+@Injectable()
+export class ApiKeyService {
+  private repository: Repository<ApiKeyEntity>
+
+  constructor(@Inject(ApiKeyLogService) private readonly apiKeyLogService: ApiKeyLogService) {
+    this.repository = AuthenticationModule.connection.getRepository(ApiKeyEntity)
+  }
+
+  async generate(data: GenerateApiKeyRequest, options?: ApiKeyOptionsRequest, apiSecretKey?: string): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    const apiKeyEntity = this.repository.create({
+      ...data,
+      secretKey: bcrypt.hashSync(apiSecretKey ?  apiSecretKey : randomUUID(), 10)
+    })
+    const entitySaved = await this.repository.save(apiKeyEntity)
+
+    const { API_KEY_GENERATED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_GENERATED.code,
+        eventMessage: API_KEY_GENERATED.message,
+        apiKeyId: entitySaved.id,
+        ...options
+    })
+
+    return [entitySaved, null]
+  }
+
+  async regenerate(apiKeyId: string, options?: ApiKeyOptionsRequest, apiSecretKey?: string ): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    const apiKey = await this.repository.findOne({ where: { id: apiKeyId } })
+    if (!apiKey) return [null, ErrorCode.API_KEY_NOT_FOUND]
+
+    apiKey.secretKey = await bcrypt.hash(apiSecretKey ? apiSecretKey : randomUUID(), 10)
+
+    const entitySaved = await this.repository.save(apiKey)
+
+    const { API_KEY_REGENERATED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_REGENERATED.code,
+        eventMessage: API_KEY_REGENERATED.message,
+        apiKeyId,
+        ...options
+    })
+
+    return [entitySaved, null]
+  }
+
+  async activate(apiKeyId: string, options?: ApiKeyOptionsRequest): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    const apiKey = await this.repository.findOne({ where: { id: apiKeyId } })
+    if (!apiKey) return [null, ErrorCode.API_KEY_NOT_FOUND]
+
+    apiKey.isActive = true
+
+    const entitySaved = await this.repository.save(apiKey)
+
+    const { API_KEY_ACTIVATED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_ACTIVATED.code,
+        eventMessage: API_KEY_ACTIVATED.message,
+        apiKeyId,
+        ...options
+    })
+
+    return [entitySaved, null]
+  }
+
+  async deactivate(apiKeyId: string, options?: ApiKeyOptionsRequest): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    const apiKey = await this.repository.findOne({ where: { id: apiKeyId } })
+    if (!apiKey) return [null, ErrorCode.API_KEY_NOT_FOUND]
+
+    apiKey.isActive = false
+
+    const entitySaved = await this.repository.save(apiKey)
+
+    const { API_KEY_DEACTIVATED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_DEACTIVATED.code,
+        eventMessage: API_KEY_DEACTIVATED.message,
+        apiKeyId,
+        ...options
+    })
+
+    return [entitySaved, null]
+  }
+
+  async revoke(apiKeyId: string, options?: ApiKeyOptionsRequest): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    const apiKey = await this.repository.findOne({ where: { id: apiKeyId } })
+    if (!apiKey) return [null, ErrorCode.API_KEY_NOT_FOUND]
+
+    const removedEntity = await this.repository.remove(apiKey)
+
+    const { API_KEY_REVOKED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_REVOKED.code,
+        eventMessage: API_KEY_REVOKED.message,
+        apiKeyId,
+        ...options
+    })
+
+    return [removedEntity, null]
+  }
+
+  async validate(publicKey?: string, secretKey?: string, options?: ApiKeyOptionsRequest): Promise<[ApiKeyEntity, ErrorCode | undefined]> {
+    if(!publicKey && !secretKey) return [null,
+        ErrorCode.PUBLIC_KEY_AND_SECRET_KEY_NOT_GIVEN]
+
+    const apiKey = await this.repository.findOne({ where: { secretKey  } })
+    if (!apiKey) return [null, ErrorCode.API_KEY_NOT_FOUND]
+
+    if (!apiKey.isActive) return  [null, ErrorCode.API_KEY_DEACTIVATED]
+
+    const { API_KEY_VALIDATED } = ApiKeyLogEvents
+
+    this.apiKeyLogService.register({
+        eventCode: API_KEY_VALIDATED.code,
+        eventMessage: API_KEY_VALIDATED.message,
+        apiKeyId: apiKey.id,
+        ...options
+    })
+
+    return [apiKey, null]
+  }
+}
