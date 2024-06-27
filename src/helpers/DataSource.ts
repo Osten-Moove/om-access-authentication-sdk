527037668448750b90a681bdc5bc869fdbd41767
vanei2002 <vanei.jesus016@gmail.com>
2024-06-27 15:57:34 -0300

    feat: Add ApiKeyEntity, ApiKeyLogEntity, ApiKeyService, and ApiKeyLogService

--- src/helpers/DataSource.ts
+++ src/helpers/DataSource.ts
@@ -1,12 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm'
 import { LoginEntity, LoginLogEntity } from '../entities'
 import { AuthorizationLibDefaultOwner } from '../helpers/AuthorizationLibVariables'
+import { ApiKeyEntity } from '../entities/ApiKeyEntity'
+import { ApiKeyLogEntity } from '../entities/ApiKeyLogEntity'
 
 export class AuthenticationDataSource extends DataSource {
   constructor(database: DataSourceOptions) {
     super({
       ...database,
-      entities: [LoginEntity, LoginLogEntity],
+      entities: [LoginEntity, LoginLogEntity, ApiKeyEntity, ApiKeyLogEntity],
       name: AuthorizationLibDefaultOwner,
     })
   }
