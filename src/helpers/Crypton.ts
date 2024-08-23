60c5dbe515409066c7ef5aea1c158adc239dbce6
vanei2002 <vanei.jesus016@gmail.com>
2024-08-23 12:34:39 -0300

    (refact): change file cryoton.ts

--- src/helpers/Crypton.ts
+++ src/helpers/Crypton.ts
@@ -2,21 +2,22 @@ import * as crypto from 'crypto';
 
 export class CryptonSecurity {
   static algorithm = 'aes-256-cbc';
-  static iv = '1234567890abcdef1234567890abcdef'; // 16 bytes em hexadecimal
+  static iv = '1234567890abcdef1234567890abcdef';
 
+  static key(key: string): Buffer {
+    return crypto.createHash('sha256').update(key).digest();
+  }
   static encrypt(secretKey: string, encryptionKey: string): string {
-    const key = crypto.createHash('sha256').update(encryptionKey).digest();
 
-    const cipher = crypto.createCipheriv(this.algorithm, key, Buffer.from(this.iv, 'hex'));
+    const cipher = crypto.createCipheriv(this.algorithm, this.key(encryptionKey), Buffer.from(this.iv, 'hex'));
     let encrypted = cipher.update(secretKey, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
   }
 
   static decrypt(encryptedSecretKey: string, encryptionKey: string): string {
-    const key = crypto.createHash('sha256').update(encryptionKey).digest();
 
-    const decipher = crypto.createDecipheriv(this.algorithm, key, Buffer.from(this.iv, 'hex'));
+    const decipher = crypto.createDecipheriv(this.algorithm, this.key(encryptionKey), Buffer.from(this.iv, 'hex'));
     let decrypted = decipher.update(encryptedSecretKey, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
